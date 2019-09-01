093aca8c079ca55473ba3ac465381399b01af9e3
Tomasz Strojny <tomasz@init.biz>
2019-09-01 10:01:31 +0200

    Improve docs and add formatdot method

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -28,17 +28,43 @@ class Helpers
      */
     public static function formatMoney(int $amount, string $currencyCode): String
     {
-        $currency = Currency::findByCode($currencyCode);
-        
-        $fractionDigits = $currency->initbiz_money_fraction_digits;
-
         $currencyHelper = new CurrencyHelper();
 
-        $value = $currencyHelper->format($amount / pow(10, $fractionDigits), ['in' => $currencyCode]);
+        $value = $currencyHelper->format(self::formatAmountDot($amount, $currencyCode), ['in' => $currencyCode]);
 
         return $value;
     }
 
+    /**
+     * Return amount in float format (with dot separator)
+     *
+     * @param integer $amount
+     * @param string $currencyCode
+     * @return String
+     */
+    public static function formatAmountDot(int $amount, string $currencyCode): String
+    {
+        $currency = Currency::findByCode($currencyCode);
+        
+        $fractionDigits = $currency->initbiz_money_fraction_digits;
+
+        $dotAmount = $amount / pow(10, $fractionDigits);
+
+        $dotAmount = (string)$dotAmount;
+
+        return $dotAmount;
+    }
+
+    /**
+     * Format money using array where 
+     * [
+     *    'amount' => <amount>,
+     *    'currency' => <currency_code>,
+     * ]
+     *
+     * @param array $params
+     * @return String
+     */
     public static function formatMoneyBoth(array $params): String
     {
         if (empty($params)) {
@@ -49,4 +75,26 @@ class Helpers
 
         return $value;
     }
+
+    /**
+     * Return amount in float format (with dot separator) using array where
+     * [
+     *    'amount' => <amount>,
+     *    'currency' => <currency_code>,
+     * ]
+     *
+     * @param array $params
+     * @return String
+     */
+    public static function formatAmountDotBoth(array $params): String
+    {
+        if (empty($params)) {
+            return '';
+        }
+
+        $value = self::formatAmountDot($params['amount'], $params['currency']);
+
+        return $value;
+    }
+
 }
